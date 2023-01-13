8f8c622ef91fa7e66e0eb9238794533c00cc39af
Cole Waggoner <waggcj02@gmail.com>
2023-01-13 14:28:29 -0500

    Builds recipe column and pulls local restaurant info for future cards

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // Variables
 var searchbtn = document.getElementById("search")
 var city = document.querySelector("#cityName")
+var cardList = document.querySelector(".recCard")
+var body = document.querySelector('.body')
+  body.style.backgroundColor = '#788585'
 
 
   //navbar 
@@ -27,34 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
  
 
-
-
-/*link for finding geoID by city (geoid found at data/typehead/results/3/detailsV2/locationId)    
-(lat and long found at data/typehead/results/3/detailsV2/geocode/lat, long)*/
-
-getLocation = (city) => {
-  var URL = 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=' + city + '&lang=en_US&units=mi'
-
-  const cityoptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '92892adfc3msh6c3519920a0dd57p1c0fcbjsn891d97cf349d',
-      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-    }
-  };
-
-  fetch(URL, cityoptions)
-    .then(function (response) {
-     
-      return response.json()
-    })
-    .then(function (data) {
-      console.log(data)
-      getprice(data)
-      
-      
-    })
-}
   
 
 
@@ -108,10 +83,10 @@ getprice = (id) => {
 
 
 
-//link for finding resaurants 
+//link for finding recipes 
   getRecID = (foodType) => {
 
-  fetch('https://api.spoonacular.com/recipes/complexSearch?query='+ foodType +'&number=10&apiKey=965b1b05045a4605a4a66144db0c2500')
+  fetch('https://api.spoonacular.com/recipes/complexSearch?query='+ foodType +'&number=1&apiKey=965b1b05045a4605a4a66144db0c2500')
     .then(function (response) {
      
       return response.json()
@@ -126,7 +101,7 @@ getprice = (id) => {
 
 getRecipe = (foodData) => {
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1; i++) {
   var recID = foodData.results[i].id
   console.log(recID)
 
@@ -142,11 +117,97 @@ getRecipe = (foodData) => {
 
     appendLink = (recInfo) => {
       var title = recInfo.title
-      var pic = recInfo.image
+      var picdata = recInfo.image
       var link = recInfo.sourceUrl
 
+      var mainCard = document.createElement('div')
+        //styling element
+         mainCard.style.backgroundColor = '#ede9d8'
+         mainCard.style.boxShadow = '4px 4px 12px rgba(0, 0, 0, .4)'
+         mainCard.style.borderRadius = '20px'
+         mainCard.style.margin = '25px'
+         mainCard.style.padding = '15px'
+
+
+      //adding image to the card
+      var cardImage = document.createElement('div')
+        cardImage.style.padding = '10px'
+        
+        
+      var pic = document.createElement('img')
+        pic.style.borderRadius = '20px'
+        pic.style.maxHeight = '100%'
+        pic.style.maxWidth = '100%'
+        
+
+      pic.src = picdata
+
+      cardImage.appendChild(pic)
+      mainCard.append(cardImage)
+
+      //adding text and link to the card
+
+      var content = document.createElement('div')
+      var recLink = document.createElement('a')
+        recLink.style.textDecoration = 'none'
+        recLink.style.color = 'black'
+
+      recLink.textContent = title
+      recLink.setAttribute("href", link)
+      content.appendChild(recLink)
+      
+
+      mainCard.append(content)
+
+      cardList.append(mainCard)
+        
+
+  }}}
+
+
+  /*link for finding geoID by city (geoid found at data/typehead/results/3/detailsV2/locationId)    
+(lat and long found at data/typeahead/results/3/detailsV2/geocode/lat, long)*/
+
+getLocation = (city) => {
+  var URL = 'https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=' + city + '&lang=en_US&units=mi'
+
+  const cityoptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '92892adfc3msh6c3519920a0dd57p1c0fcbjsn891d97cf349d',
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
     }
-  }
+  };
+
+  fetch(URL, cityoptions)
+    .then(function (response) {
+     
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data)
+      getRestaurant(data)
+      
+    })
+}
+
+getRestaurant = (cityInfo) => {
+  var lat = cityInfo.data.Typeahead_autocomplete.results[3].detailsV2.geocode.latitude
+  var long = cityInfo.data.Typeahead_autocomplete.results[3].detailsV2.geocode.longitude
+  console.log(lat)
+  console.log(long)
+
+  fetch('https://api.spoonacular.com/food/restaurants/search?cuisine=italian&lat=' + lat + '&lng=' + long + '&apiKey=965b1b05045a4605a4a66144db0c2500')
+  .then(function (response) {
+   
+    return response.json()
+  })
+  .then(function (recData) {
+    console.log(recData)
+    // appendResCard(recData)
+  })
+
+
 
 }
 
@@ -155,9 +216,8 @@ getRecipe = (foodData) => {
     searchbtn.addEventListener("click", function (event) {
       event.preventDefault()
     
-      getRecID(city.value)
+      // getRecID(food.value)
 
-      // getLocation(city.value)
+      getLocation(city.value)
     
-      console.log(city.value)
     })
